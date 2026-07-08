6cb5a5097ec63cfe8d2f6fdbd9d632135bc05167
Danylo <danylo@MacBook-Air.local>
2026-07-08 08:38:51 +0300

    Промежуточный результат задания 3

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,4 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import Counter from "./ParentComponent";
-import Hello from "./ChildComponent";
-import Redev from "./SiblingComponent";
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Counter />
-    <Redev />
-  </StrictMode>,
-);
+
+createRoot(document.getElementById("root")).render(<StrictMode></StrictMode>);
